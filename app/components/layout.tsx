7fc1b4130cc0ff3313c49a4d9ca7c62874c88f07
Dhayvison <dhayvisonbraga@hotmail.com>
2022-10-27 20:15:30 -0300

    feat: invert logo image on dark theme

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -25,9 +25,15 @@ export default function Layout({ title, children }: LayoutProps) {
       </Head>
       <Navbar>
         <Navbar.Brand>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            width={72}
+            height={16}
+            style={{ filter: isDark ? "invert(1)" : "" }}
+          />
           <Text b color="inherit" hideIn="xs" css={{ ml: 8 }}>
-            ACME
+            Blog
           </Text>
         </Navbar.Brand>
         <Navbar.Content enableCursorHighlight variant="underline">
