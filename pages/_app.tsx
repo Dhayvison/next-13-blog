88653841a3a839c508b0172c1f14baf06fa09890
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2022-10-27 13:21:43 -0300

    feat: config theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
+const lightTheme = createTheme({
+  type: "light",
+  theme: {},
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
